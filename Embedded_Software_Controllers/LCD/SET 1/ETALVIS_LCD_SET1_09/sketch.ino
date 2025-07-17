ffb0cced2051a784ec8cfaa29ad6a5fa8ea4503f
Bajatinti Anwar <anwarbajarinti@gmail.com>
2025-07-17 19:15:41 +0530

    update

---
+++ Embedded_Software_Controllers/LCD/SET 1/ETALVIS_LCD_SET1_09/sketch.ino
@@ -0,0 +1,68 @@
+volatile char *porta = (char *)0x22;
+volatile char *portb = (char *)0x25;
+volatile char *ddra  = (char *)0x21;
+volatile char *ddrb  = (char *)0x24;
+
+#define RS 1
+#define E  0
+
+void delay_(unsigned int t)
+{
+  for (volatile long i = 0; i < t * 1000; i++);
+}
+
+void pulseEnable()
+{
+  *portb |= (1 << E);
+  delay_(1);
+  *portb &= ~(1 << E);
+}
+
+void command(char cmd)
+{
+  *portb &= ~(1 << RS);   // RS = 0 for command
+  *porta = cmd;
+  pulseEnable();
+  delay_(2);
+}
+
+void data(char d)
+{
+  *portb |= (1 << RS);    // RS = 1 for data
+  *porta = d;
+  pulseEnable();
+  delay_(2);
+}
+
+void setup()
+{
+  *ddra = 0xFF;  // PORTA for data output
+  *ddrb |= (1 << RS) | (1 << E); // RS and E as output
+
+  command(0x38);  // Function set: 8-bit, 2-line
+  command(0x0E);  // Display ON, cursor ON
+  command(0x01);  // Clear display
+  command(0x06);  // Entry mode set: increment
+  
+}
+
+void loop()
+{
+  char name[] = "WELCOME";
+  int len = 7;  // Length of "WELCOME"
+
+  
+  for (char k = 0; k <=9; k++)
+  {
+    command(0x01);          // Clear screen
+    command(0xC0 + k);      // Set cursor position on Line 2
+
+    for (int i = 0; i < len; i++) {
+      data(name[i]);        // Display each character
+    }
+
+    delay_(2000);            // Wait before shifting
+  }
+
+  while (1);  // Stay forever
+}
---
+++ Embedded_Software_Controllers/LCD/SET 2/ETALVIS_LCD_SET2_1/sketch.ino
@@ -0,0 +1,70 @@
+volatile char *porta = (char *)0x22;
+volatile char *portb = (char *)0x25;
+volatile char *ddra  = (char *)0x21;
+volatile char *ddrb  = (char *)0x24;
+
+#define RS 1
+#define E  0
+
+void delay_(unsigned int t)
+{
+  for (volatile long i = 0; i < t * 1000; i++);
+}
+
+void pulseEnable()
+{
+  *portb |= (1 << E);
+  delay_(1);
+  *portb &= ~(1 << E);
+}
+
+void command(char cmd)
+{
+  *portb &= ~(1 << RS);   // RS = 0 for command
+  *porta = cmd;
+  pulseEnable();
+  delay_(2);
+}
+
+void data(char d)
+{
+  *portb |= (1 << RS);    // RS = 1 for data
+  *porta = d;
+  pulseEnable();
+  delay_(2);
+}
+
+void setup()
+{
+  *ddra = 0xFF;  // PORTA for data output
+  *ddrb |= (1 << RS) | (1 << E); // RS and E as output
+
+  command(0x38);  // Function set: 8-bit, 2-line
+  command(0x0E);  // Display ON, cursor ON
+  command(0x01);  // Clear display
+  command(0x06);  // Entry mode set: increment
+  
+}
+
+void loop()
+{
+  char name[] = "WELCOME";
+  char name2[] = "ETALVIS";
+  int len1 = 7;
+  int len2 = 7;
+
+  for (char k = 0; k <= 9; k++)  // Scroll WELCOME left
+  {
+    command(0x01);            // Clear screen
+
+    // Print WELCOME on Line 1 (0x80)
+    command(0x80 + k);
+    for (int i = 0; i < len1; i++) {
+      data(name[i]);
+    }
+
+    delay_(2000);
+  }
+
+  
+}
